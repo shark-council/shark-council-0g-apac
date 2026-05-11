d361eeb7f0ad2b450643311c6c7d5baa7771e29e
kiv1n <aloha@kiv1n.ru>
2026-05-11 14:40:27 +0300

    Add app configuration, global styles, and favicon; remove old favicon

---
+++ app/config/app.ts
@@ -0,0 +1,11 @@
+export const appConfig = {
+  name: "Shark Council",
+  edition: "0G APAC",
+  description:
+    "Let 0G Sharks, built by elite 0G developers, debate and execute your trade ideas",
+  gitHubUrl: "https://github.com/shark-council/shark-council-0g-apac",
+  developer: {
+    name: "@kiv1n",
+    url: "https://x.com/kiv1n",
+  },
+} as const;
