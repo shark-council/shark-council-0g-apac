46d2d6a195249e21a61ec5166c4e742dbd70d0da
kiv1n <aloha@kiv1n.ru>
2026-05-11 15:18:55 +0300

    Add hero image and update IndexPage layout with title and skeleton

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,32 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
+import Image from "next/image";
+
+// TODO: Add a form
 export default function IndexPage() {
-  return <main className="flex flex-1 flex-col items-center px-4 py-4"></main>;
+  return (
+    <main className="flex flex-1 flex-col items-center px-4 py-4">
+      <div className="max-w-2xl">
+        {/* Hero */}
+        <Image
+          src="/images/hero.gif"
+          alt="Hero"
+          priority={false}
+          width="100"
+          height="100"
+          sizes="100vw"
+          className="w-full rounded-md"
+        />
+        {/* Title */}
+        <h1 className="text-3xl font-semibold tracking-tight text-balance text-center mt-8">
+          Let <span className="text-primary">0G Sharks</span>, built by elite{" "}
+          <span className="text-accent">0G Developers</span>, debate and execute
+          your trade ideas
+        </h1>
+        {/* Form */}
+        <Skeleton className="h-12 mt-8" />
+      </div>
+    </main>
+  );
 }
