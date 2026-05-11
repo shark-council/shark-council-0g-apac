cdb7042100d5b4609ac94514130ab73e7e85f925
kiv1n <aloha@kiv1n.ru>
2026-05-11 15:19:05 +0300

    Correct capitalization in app description for consistency

--- app/config/app.ts
+++ app/config/app.ts
@@ -2,7 +2,7 @@ export const appConfig = {
   name: "Shark Council",
   edition: "0G APAC",
   description:
-    "Let 0G Sharks, built by elite 0G developers, debate and execute your trade ideas",
+    "Let 0G Sharks, built by elite 0G Developers, debate and execute your trade ideas",
   gitHubUrl: "https://github.com/shark-council/shark-council-0g-apac",
   developer: {
     name: "@kiv1n",
