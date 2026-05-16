b8d6d05b5c365df0ed36f3715923873e737d8a4d
kiv1n <aloha@kiv1n.ru>
2026-05-16 07:37:43 +0300

    Refactor 0G configuration: add type definitions and separate testnet configuration

--- app/config/0g.ts
+++ app/config/0g.ts
@@ -1,6 +1,22 @@
-import { defineChain } from "viem";
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Chain, defineChain } from "viem";
+
+type zerogConfig = {
+  chain: Chain;
+  contracts: {
+    agenticIdentity: `0x${string}`;
+  };
+  compute: {
+    model: string;
+    baseUrl: string;
+  };
+  storage: {
+    rpcUrl: string;
+    indexerRpc: string;
+  };
+};
+
 const zerogTestnet = defineChain({
   id: 16602,
   name: "0G Testnet",
@@ -37,10 +53,24 @@ const zerogMainnet = defineChain({
   testnet: false,
 });
 
-export const zerogConfig = {
+const zerogConfigTestnet: zerogConfig = {
+  chain: zerogTestnet,
+  contracts: {
+    agenticIdentity: "0xad946b989a831eea0bb8d9c62b7ef34487996fdb",
+  },
+  compute: {
+    model: "qwen/qwen-2.5-7b-instruct",
+    baseUrl: "https://router-api-testnet.integratenetwork.work/v1",
+  },
+  storage: {
+    rpcUrl: "https://evmrpc-testnet.0g.ai",
+    indexerRpc: "https://indexer-storage-testnet-turbo.0g.ai",
+  },
+};
+
+const zerogConfigMainnet: zerogConfig = {
   chain: zerogMainnet,
   contracts: {
-    // agenticIdentity: "0xad946b989a831eea0bb8d9c62b7ef34487996fdb", // Testnet
     agenticIdentity: "0x3fd101f7722e9b4168bff1ea17d51759196ea80d",
   },
   // TODO: Replace with mainnet
@@ -53,4 +83,6 @@ export const zerogConfig = {
     rpcUrl: "https://evmrpc-testnet.0g.ai",
     indexerRpc: "https://indexer-storage-testnet-turbo.0g.ai",
   },
-} as const;
+};
+
+export const zerogConfig = zerogConfigMainnet;
